034d8145e2e6d81d79b83047b744289bd2a8a626
Elad Sopher <127190903+EladSopher@users.noreply.github.com>
2025-04-04 13:01:03 +0300

    Add files via upload

---
+++ Code/statuspage_app/statuspage/subscribers/tests.py
@@ -0,0 +1,6 @@
+from django.test import TestCase
+
+class SmokeTestCase(TestCase):
+    def test_basic_truth(self):
+        """A simple test to ensure the testing framework is working."""
+        self.assertTrue(True)
